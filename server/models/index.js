fb2924aed76f105ce6dfeed571c6e7818bd58608
Buck <ryan42buck@gmail.com>
2022-11-26 18:17:10 -0500

    models index. models should be done

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const Category = require("./Category");
+const User = require("./User");
+const Game = require("./Game");
+const Review = require("./Review");
+
+module.exports = { Category, User, Game, Review };
