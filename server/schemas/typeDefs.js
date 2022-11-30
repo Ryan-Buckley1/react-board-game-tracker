5571e3708b8a6c277dff7ba6a2f4eca32ea95cb6
Buck <ryan42buck@gmail.com>
2022-11-29 22:54:04 -0500

    added most of typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,6 +5,73 @@ const typeDefs = gql`
     _id: ID!
     username: String!
     email: String!
+    favGames: [Game]
+    favCount: String
+    wishGames: [Game]
+    wishCount: String
+    ownGames: [Game]
+    ownCount: String
+    reviews: [Review]
+    reviewCount: String
+  }
+
+  type Game {
+    _id: ID!
+    name: String!
+    description: String!
+    min_players: String!
+    max_players: String!
+    duration: String!
+    age_rating: String!
+    image_url: String!
+    categories: [Category]
+    categoryCount: String
+    reviews: [Review]
+    reviewCount: String
+  }
+
+  type Category {
+    _id: ID!
+    name: String!
+  }
+
+  type Review {
+    _id: ID!
+    author: String!
+    reviewText: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input GameInput {
+    name: String!
+    description: String
+    min_players: String
+    max_players: String
+    duration: String
+    age_rating: String
+    image_url: String
+    categories: [Category]
+  }
+
+  type Query {
+    currentUser: User
+    user: User
+    users: [User]
+    game(name: String!): Game
+    games: [Game]
+    category(name: String!): Category
+    categories: [Category]
+  }
+
+  type Mutation {
+    login(email: String, password: String): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addCategory(name: String!): Category
+    removeCategory(name: String!): Category
   }
 `;
 
